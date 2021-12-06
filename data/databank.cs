92e8ccd8966c4a3a6ef708bc2233b99617567a5e
elmira68 <irandoostelmira@yahoo.com>
2021-12-06 13:07:14 +0200

    add file

---
+++ data/databank.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace ATM
+{
+    
+public class MyData
+{
+    public int id;
+    public string name;
+    public string cardNumber;
+    public double mojodi;
+    public int pass;
+    public string bankName;
+
+    public static List<MyData> listBank = new List<MyData>();
+
+    static int count = 0;
+
+    public static void create()
+    {
+        MyData dataBank = new MyData();
+        dataBank.id = count;
+        dataBank.name = "MH Mehryar";
+        dataBank.cardNumber = "6104337836540649";
+        dataBank.mojodi = 1000000;
+        dataBank.pass = 1234;
+        dataBank.bankName = "Mellat";
+
+
+        listBank.Add(dataBank);
+
+
+        MyData dataBank1 = new MyData();
+        dataBank1.id = count;
+        dataBank1.name = "elmira irandoost";
+        dataBank1.cardNumber = "5859831194826177";
+        dataBank1.mojodi = 5000000;
+        dataBank1.pass = 1368;
+        dataBank1.bankName = "tejarat";
+
+        listBank.Add(dataBank1);
+    }
+
+
+
+}
+}
---
+++ data/interface/interface.cs
@@ -0,0 +1,16 @@
+using System;
+namespace ATM
+{
+    public interface IMyInterface
+{
+    // public bool check(int pass,string bankName);
+    public bool check();
+    public void showMenu(bool c);
+    public void mojodi();
+    public double bardasht();
+    public double enteghal();
+    public void changePass();
+    public void log();
+
+}
+}
